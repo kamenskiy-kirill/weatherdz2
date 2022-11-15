249b2883d6b4ec79d20acab57da988718c53702a
kamenskiy-kirill <116569605+kamenskiy-kirill@users.noreply.github.com>
2022-11-15 19:57:51 +0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import math
+
+print("Введите коэффициенты для уравнения")
+print("ax^2 + bx + c = 0:")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+
+dis = b ** 2 - 4 * a * c
+print("Дискриминант D = %.2f" % dis)
+
+if dis > 0:
+    x1 = (-b + math.sqrt(dis)) / (2 * a)
+    x2 = (-b - math.sqrt(dis)) / (2 * a)
+    print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+elif dis == 0:
+    x = -b / (2 * a)
+    print("x = %.2f" % x)
+else:
+    print("Корней нет")
